185dea0bec9ccd46c276d22474c83d4ba6270c91
JingyiMeng6 <60023395+JingyiMeng6@users.noreply.github.com>
2023-04-09 19:07:44 -0700

    add static form in login and signup screen

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "../header";
 
 const LoginComponent = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <>
       <Header />
       <h4>Login</h4>
+      <div className="form-group">
+        <label>Username</label>
+        <input
+            type="text"
+            className="form-control"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+            type="password"
+            className="form-control"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+        />
+      </div>
+      <button className="btn btn-primary">
+        Login
+      </button>
     </>
   );
 };
--- src/components/signup/index.js
+++ src/components/signup/index.js
@@ -1,11 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "../header";
 
 const SignupComponent = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
   return (
     <>
       <Header />
       <h4>Signup</h4>
+      <div className="form-group">
+        <label>Username</label>
+        <input
+            type="text"
+            className="form-control"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+            type="password"
+            className="form-control"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+        />
+      </div>
+      <div className="form-group">
+        <label>Repeat Password</label>
+        <input
+            type="password"
+            className="form-control"
+            value={repeatPassword}
+            onChange={(e) => {
+              setRepeatPassword(e.target.value);
+            }}
+        />
+      </div>
+      <button className="btn btn-primary">
+        Sign up!
+      </button>
     </>
   );
 };
