ab0aba16e0abdef31bfea89b18b294ad9c7a5434
Han Bao <baaaohaaan@outlook.com>
2023-04-14 13:15:28 -0700

    Remove redundant code

--- src/components/details/local-item.js
+++ src/components/details/local-item.js
@@ -1,14 +1,7 @@
 import React from "react";
 import "./index.css";
-import {useNavigate} from "react-router";
-import {Link} from "react-router-dom";
 
 const LocalItem = () => {
-    let navigate = useNavigate();
-    const routeChange = () => {
-        let path = `../profile`;
-        navigate(path);
-    }
     const description = "Discusses Web development for sites that are dynamic, " +
         "data driven, and interactive. Focuses on the software development issues " +
         "of integrating multiple languages, assorted data technologies, and Web interaction. " +
@@ -20,28 +13,13 @@ const LocalItem = () => {
         "accomplishments, and user guidelines. All source files must be open and be automatically " +
         "served by a sources server."
 
-    // const profile = useSelector(state => state.profile);
     return(
         // content in the middle
         <div className="col-10 col-md-8 col-lg-6 col-xl-6">
-            <div className="row">
-                {/*<div className="col-1">*/}
-                {/*    <i className="bi bi-x-lg" onClick={() => routeChange()}/>*/}
-                {/*</div>*/}
-                {/*<div className="col-11">*/}
-                {/*    <div className="fw-bold" style={{fontSize: 20}}>CS 5610 - 06</div>*/}
-                {/*    <div className="fw-semibold" style={{fontSize: 20}}>Web Development</div>*/}
-                {/*</div>*/}
-            </div>
             <div className="position-relative">
                 <img className="profile-banner-relative-pos" src={`/images/polyglot.png`} height={300} alt="profile-banner"/>
-                {/*<img  className="position-absolute headshot-relative-pos rounded-circle " height={150} width={150} src={`/images/jose.jpg`} alt="headshot"/>*/}
-                {/*<Link to="/tuiter/edit-profile" className="position-absolute btn btn-outline-dark rounded-pill button-relative-pos fw-bold">Edit profile</Link>*/}
             </div>
 
-            {/*<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>*/}
-
-            {/*<div className="ms-4 fw-bold" style={{fontSize: 20}}>CS 5610 - 06</div>*/}
             <div className="ms-2 fw-bold" style={{fontSize: 20}}>CS 5610</div>
             <div className="ms-2 fw-semibold" style={{fontSize: 20}}>Web Development</div>
             <br/>
@@ -66,25 +44,6 @@ const LocalItem = () => {
             <div className="ms-4 me-3 fw-normal">The professor is amazing!</div>
             <div className="ms-3 me-3 fw-semibold">Student3</div>
             <div className="ms-4 me-3 fw-normal">Highly recommended!</div>
-            {/*<div className="row mt-3 ms-4 me-5" style={{color:'gray'}}>*/}
-            {/*    <div className="col-4">*/}
-            {/*        <i className="bi bi-pin-map" /> Boston*/}
-            {/*    </div>*/}
-            {/*    <div className="col-4">*/}
-            {/*        <i className="bi bi-balloon" /> Spring 2023*/}
-            {/*    </div>*/}
-            {/*    <div className="col-4">*/}
-            {/*        <i className="bi bi-calendar-event"/> 01/2023*/}
-            {/*    </div>*/}
-            {/*</div>*/}
-            {/*<div className="row mt-2 ms-3">*/}
-            {/*    <div className="col-3">*/}
-            {/*        <p className="fw-bold">100<span className="fw-light" style={{color:'gray'}}> Following</span></p>*/}
-            {/*    </div>*/}
-            {/*    <div className="col-3">*/}
-            {/*        <p className="fw-bold">200<span className="fw-light" style={{color:'gray'}}> Followers</span></p>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
         </div>
     );
 };
