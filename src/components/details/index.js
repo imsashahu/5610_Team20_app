6220417018bddbcb6225086b7897843c47fb0ffd
Han Bao <baaaohaaan@outlook.com>
2023-04-15 12:27:17 -0700

    Move the main content of Details page to the middle

--- src/components/details/index.js
+++ src/components/details/index.js
@@ -8,7 +8,18 @@ const DetailsComponent = () => {
             <Header />
             <h4>Details</h4>
             <div className="row mt-2">
+                {/*left place holder*/}
+                <div className="col-1 col-md-2 col-lg-3 col-xl-3">
+
+                </div>
+
+                {/*content in the middle*/}
                 <LocalItem/>
+
+                {/*right place holder*/}
+                <div className="col-1 col-md-2 col-lg-3 col-xl-3">
+
+                </div>
             </div>
         </>
     );
