ad66f51ef55c9738127a62bc7e1653fa7095dc6d
JingyiMeng6 <60023395+JingyiMeng6@users.noreply.github.com>
2023-04-14 23:19:25 -0700

    edit login page look

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import Header from "../header";
 import { useDispatch, useSelector } from "react-redux";
 import { loginThunk } from "../../services/users/users-thunks";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const LoginComponent = () => {
   const { currentUser } = useSelector((state) => state.users);
@@ -11,6 +14,7 @@ const LoginComponent = () => {
   const login = () => {
     try {
       dispatch(loginThunk({ username, password }));
+      toast("Logged in successfully!");
       // navigate("/profile");
     } catch (err) {
       console.log("error");
@@ -23,9 +27,9 @@ const LoginComponent = () => {
     <>
       <Header />
       <div className="container">
-        <div className="fw-bold mt-2 mb-5">
-          <h3>Login</h3>
-        </div>
+        {/*<div className="fw-bold mt-2 mb-5">*/}
+        {/*  <h3>Login with username and password.</h3>*/}
+        {/*</div>*/}
         <div className="form-group">
           <label>Username</label>
           <input
@@ -48,10 +52,16 @@ const LoginComponent = () => {
             }}
           />
         </div>
-        <button onClick={login} className="btn btn-warning mt-2">
-          Login
-        </button>
+        <div className={"d-flex justify-content-center"}>
+          <button onClick={login} className="btn btn-warning mt-2">
+            Login
+          </button>
 
+        </div>
+        <div className={"d-flex justify-content-center text-secondary"}>
+          Don't have an account? Click Sign-up!
+        </div>
+        <br/>
         <div>
           {currentUser && (
             <div>
@@ -62,6 +72,7 @@ const LoginComponent = () => {
           )}
         </div>
       </div>
+      <ToastContainer />
     </>
   );
 };
