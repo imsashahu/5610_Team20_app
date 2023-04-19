7f4519f5a1e4d4dd9a681c0f1c9835477367c02b
ziweiwang98 <wang.ziwei2@northeastern.edu>
2023-04-19 02:01:07 -0700

    Drafted headline for home page

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,18 +1,27 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import Header from "../header";
-import {useDispatch} from "react-redux";
-import {profileThunk} from "../../services/users/users-thunks";
+import { useDispatch } from "react-redux";
+import { profileThunk } from "../../services/users/users-thunks";
 
 const HomeComponent = () => {
   const dispatch = useDispatch();
-  useEffect(()=> {dispatch(profileThunk());
-  },[]);
+  useEffect(() => {
+    dispatch(profileThunk());
+  }, []);
   return (
     <>
       <Header />
       <div className="container">
         <div className="fw-bold mt-2 mb-5">
-          <h3>Home</h3>
+          <div className="fs-1 d-flex justify-content-center align-items-center">
+            Rate Your Courses
+          </div>
+          <div className="fs-1 d-flex justify-content-center align-items-center">
+            Find Your Next Courses
+          </div>
+          <div className="fs-1 d-flex justify-content-center align-items-center">
+            Boost Your Learning
+          </div>
         </div>
       </div>
     </>
