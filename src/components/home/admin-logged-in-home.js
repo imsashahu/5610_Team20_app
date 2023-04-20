7527ebbe3ddf05f16bbade20ff64ac9c806c1607
ziweiwang98 <wang.ziwei2@northeastern.edu>
2023-04-19 23:59:47 -0700

    WIP on home page

--- src/components/home/admin-logged-in-home.js
+++ src/components/home/admin-logged-in-home.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { profileThunk } from "../../services/users/users-thunks";
 import { Link } from "react-router-dom";
+import CourseList from "./course-list";
 
 const LoggedInHome = () => {
   const { currentUser } = useSelector((state) => state.users);
@@ -14,14 +15,10 @@ const LoggedInHome = () => {
   return (
     <>
       {" "}
-      <div className="d-flex flex-column justify-content-center align-items-center fs-1 mt-5">
-        <div>
-          Welcome back,{` ${currentUser.role.toLowerCase()} `}
-          <Link className="text-dark" to="/profile">
-            {currentUser.username}
-          </Link>
-        </div>
-        <div>Take a new course? Review it now!</div>
+      <div className="d-flex flex-column justify-content-center align-items-center fs-3 mt-5">
+        <div>Here is a curated list of courses for you</div>
+        <CourseList />
+        <div>TODO: implement admin logged-in home page</div>
       </div>
     </>
   );
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,22 +2,37 @@ import React, { useEffect } from "react";
 import Header from "../header";
 import { useDispatch, useSelector } from "react-redux";
 import { profileThunk } from "../../services/users/users-thunks";
-import LoggedInHome from "./logged-in-home";
+import StudentLoggedInHome from "./student-logged-in-home";
 import LoggedOutHome from "./logged-out-home";
+import { Link } from "react-router-dom";
 
 const HomeComponent = () => {
   const { currentUser } = useSelector((state) => state.users);
-  console.log("currentUser", currentUser);
+  console.log("currentUser role", currentUser);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(profileThunk());
   }, []);
+
+  currentUser &&
+    console.log("currentUser role", currentUser.role === "STUDENT");
+
   return (
     <>
       <Header />
       <div className="container">
+        <div className="d-flex flex-column justify-content-center align-items-center fs-3 mt-5">
+          <div className="fs-3">
+            Welcome back,{` ${currentUser.role.toLowerCase()} `}
+            <Link className="text-dark" to="/profile">
+              {currentUser.username}
+            </Link>
+          </div>
+        </div>
         {!currentUser && <LoggedOutHome />}
-        {currentUser && <LoggedInHome />}
+        {currentUser && currentUser.role === "STUDENT" && (
+          <StudentLoggedInHome />
+        )}
       </div>
     </>
   );
