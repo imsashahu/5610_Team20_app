292fdc821d0ef1a8b5413444fcd6589454d78f1a
ziweiwang98 <wang.ziwei2@northeastern.edu>
2023-04-23 00:48:06 -0700

    wip

--- src/components/course-review-result/index.js
+++ src/components/course-review-result/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Navigate, redirect, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { isEmpty } from "../../utils";
 
 const CourseReviewResult = ({ courseReviews }) => {
